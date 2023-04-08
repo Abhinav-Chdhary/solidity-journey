a1f1b521019c5b7bcece09c2ffb2d2710d79ae0d
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-04-08 20:06:06 +0530

    Update FinalChallenge.sol

--- BeginnerChallenges/FinalChallenge.sol
+++ BeginnerChallenges/FinalChallenge.sol
@@ -1,34 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-/*
-       REQUIREMENTS
-    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-    2. Your contract will have a mapping of addresses to balances (address => uint)
-    3. You will have a mint function that takes two parameters: an address and a value. 
-       The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
-    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the “sender”.
-    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
-       to the amount that is supposed to be burned.
-*/
-
 contract MyToken {
 
-    string public tokenName="SOLID", tokenAbbrv="SLD";
-    uint public Supply = 0;
+    string public tokenName="SOLID"; //stores token name
+    string public tokenAbbrv="SLD"; //Token abbreviation
+    uint public supply = 0; //unsigned integer to track total supply
 
-    mapping (address => uint) addToBalance;
+    mapping (address => uint) public addToBalance;
+    //mapping from an address to it's balance
 
+    //function to mint token and increment total supply, and balance.
     function mint(address add, uint value) external {
-
+        supply += value; //new supply = supply + value
+        addToBalance[add] += value; //new balance of given address = balance of address+value
     }
 
+    //function to decrement total supply and balance
     function burn(address add, uint value) external {
-        require(addToBalance[add]>value);
-        addToBalance[add] = addToBalance[add]-value;
+        if(addToBalance[add]>=value){
+            //execute only if we have equal to or more balance than the value we are asked to burn
+            supply -= value;
+            addToBalance[add] = addToBalance[add]-value;
+        }
     }
 
 }
